de3d14f07a3de5af3a60dac5f440b1bd2b222de2
Mystique <13oct08@quantumfoam.uni.cc>
2021-07-31 18:55:39 +0200

    LGTM

--- stacks/miztiik_global_args.py
+++ stacks/miztiik_global_args.py
@@ -6,5 +6,5 @@ class GlobalArgs():
     OWNER = "MystiqueAutomation"
     REPO_NAME = "eks-scheduling-with-affinity"
     SOURCE_INFO = f"https://github.com/miztiik/{REPO_NAME}"
-    VERSION = "2020-06-15"
+    VERSION = "2021-06-15"
     MIZTIIK_SUPPORT_EMAIL = ["mystique@example.com", ]
